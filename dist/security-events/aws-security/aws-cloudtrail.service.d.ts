83b525cff6def34da769c39199340e9da4809db0
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-02-05 19:44:06 +0900

    done

--- dist/security-events/aws-security/aws-cloudtrail.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class AwsCloudTrailService {
-    private configService;
-    private cloudTrailClient;
-    constructor(configService: ConfigService);
-    getRecentSecurityEvents(): Promise<import("@aws-sdk/client-cloudtrail").Event[]>;
-}
