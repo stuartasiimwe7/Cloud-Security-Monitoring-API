f721bc8bc95b2f2ef61b28644d30e234e82a86a3
Stuart Asiimwe <stuartgabriel@ymail.com>
2025-10-07 23:22:58 +0900

    tidy up

--- dist/cloud-trail/cloud-trail.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CloudTrailService } from './cloud-trail.service';
-export declare class CloudTrailController {
-    private readonly cloudTrailService;
-    constructor(cloudTrailService: CloudTrailService);
-    testSaveEvent(eventData: any): Promise<{
-        message: string;
-        savedEvent: import("../entities/cloudTrail.entity").CloudTrailEvent;
-    }>;
-}
